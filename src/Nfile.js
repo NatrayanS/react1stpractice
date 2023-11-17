f424165c47c125ac316b44d924382ae1595f48d5
NatrayanS <s.natrayan06ayan96@gmail.com>
2023-11-18 01:04:22 +0530

    update

---
+++ src/Nfile.js
@@ -0,0 +1,12 @@
+function Ntry({deriction,dd}){
+    return <div>
+        <h1>
+            Ntry Component {deriction}! 
+        </h1>
+        <p style={{color:'red'}}>
+            lorem50 {dd}
+        </p>
+    </div>
+}
+
+export default Ntry;
